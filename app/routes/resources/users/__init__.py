5624628bedd7fef53be4774983054708f99dace4
Wesley Júnior <agarwesley19@gmail.com>
2022-08-29 11:49:07 -0300

    inicializando rotas, precisa testar

--- app/routes/resources/users/__init__.py
+++ app/routes/resources/users/__init__.py
@@ -1,4 +1,7 @@
 from app.routes.resources.users import auth
+from app.routes.resources.users import product
+from app.routes.resources.users import order
+from app.routes.resources.users import user
 
 
 def init_api(api):
@@ -6,17 +9,11 @@ def init_api(api):
     api.add_resource(auth.Register, "/auth/register")
     api.add_resource(auth.ForgetPassword, "/auth/forget-password")
 
+    api.add_resource(product.ProductList, "/products")
+    api.add_resource(product.ProductGet, "/products/<slug>")
 
-#     api = Api(app, prefix="/api")
-#     api.add_resource(auth.Login, "/auth/login")
-#     api.add_resource(auth.Register, "/auth/register")
-#     api.add_resource(auth.ForgetPassword, "/auth/forget-password")
+    api.add_resource(order.Create, "/order/create")
+    api.add_resource(order.Pay, "/order/pay")
+    api.add_resource(order.Notification, "/order/notification")
 
-#     api.add_resource(product.ProductList, "/products")
-#     api.add_resource(product.ProductGet, "/products/<slug>")
-
-#     api.add_resource(order.Create, "/order/create")
-#     api.add_resource(order.Pay, "/order/pay")
-#     api.add_resource(order.Notification, "/order/notification")
-
-#     api.add_resource(user.Orders, "/user/orders")
+    api.add_resource(user.Orders, "/user/orders")
